e9b0556c10e6590cbb7f13b707f67088ed3fc637
Hirotaka Hiraki <hiraki_uts1@yahoo.co.jp>
2022-11-19 23:42:29 +0900

    images 2022

--- generate_background_css.sh
+++ generate_background_css.sh
@@ -3,7 +3,12 @@ imagefiles=`find $dir_path -type f`
 
 image_numbers=`find $dir_path -type f | wc -l`
 floating_point_scale=5
-animation_length_in_second=$((5 * $image_numbers))
+
+animation_length_in_second=5
+animation_overlap_length_in_second=1
+animation_total_length_in_second=$(($animation_length_in_second * $image_numbers - $animation_overlap_length_in_second*($image_numbers-1)))
+# 1秒が全体の何%に当たるか
+animation_percent_by_second=`echo "scale=$floating_point_scale; 100.000 / $animation_total_length_in_second" | bc` 
 animation_frames_in_per=`echo "scale=$floating_point_scale; 100.000 / $image_numbers" | bc` 
 
 loop_count=0
@@ -18,6 +23,13 @@ do
     # set animation time
     animation_start_time=`echo "scale=$floating_point_scale; $loop_count * $animation_frames_in_per" | bc`
     animation_end_time=`echo "scale=$floating_point_scale; ($loop_count+1) * $animation_frames_in_per" | bc`
+    animation_middle_time=`echo "scale=$floating_point_scale; ($animation_start_time + $animation_end_time)/2" |bc`
+
+    animation_start_time_in_second=$(($loop_count * ($animation_length_in_second-$animation_overlap_length_in_second)))
+    animation_end_time_in_second=$((($loop_count+1) * $animation_length_in_second - $loop_count * $animation_overlap_length_in_second))
+
+    animation_start_time=`echo "scale=$floating_point_scale; $animation_start_time_in_second * $animation_percent_by_second" | bc`
+    animation_end_time=`echo "scale=$floating_point_scale; $animation_end_time_in_second * $animation_percent_by_second " | bc`
     animation_middle_time=`echo "scale=$floating_point_scale; ($animation_start_time + $animation_end_time)/2" |bc`
     
     # echo "starttime "$animation_start_time
@@ -42,7 +54,7 @@ do
         \t min-width: 100%;\n
         \t min-height: 100%;\n
         \t background: no-repeat center center;\n
-        \t animation: animation-$imagefilenameprefix $animation_length_in_second"s" ease-out infinite;\n
+        \t animation: animation-$imagefilenameprefix $animation_total_length_in_second"s" ease-out infinite;\n
         }\n
     @media screen and (min-width: 500px) {\n
         \t     .bg-container #bg-$imagefilenameprefix {\n
