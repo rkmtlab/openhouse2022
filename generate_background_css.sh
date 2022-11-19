516f73045d86550b9129027e41a65e96e2b9642f
Hirotaka Hiraki <hiraki_uts1@yahoo.co.jp>
2022-11-19 23:23:30 +0900

    images 2022

--- generate_background_css.sh
+++ generate_background_css.sh
@@ -1,22 +1,35 @@
 dir_path="assets/images/projects/mobile/*"
 imagefiles=`find $dir_path -type f`
 
-css_result=""
+image_numbers=`find $dir_path -type f | wc -l`
+floating_point_scale=5
+animation_length_in_second=$((5 * $image_numbers))
+animation_frames_in_per=`echo "scale=$floating_point_scale; 100.000 / $image_numbers" | bc` 
+
+loop_count=0
 
 
 for imagefilepath in $imagefiles;
 do
-    # echo $imagefilepath
+    # file path
     imagefilename=`echo $imagefilepath | cut -d '/' -f 5`
     imagefilenameprefix=`echo $imagefilename | cut -d '.' -f 1`
-    # echo $imagefilenameprefix
+
+    # set animation time
+    animation_start_time=`echo "scale=$floating_point_scale; $loop_count * $animation_frames_in_per" | bc`
+    animation_end_time=`echo "scale=$floating_point_scale; ($loop_count+1) * $animation_frames_in_per" | bc`
+    animation_middle_time=`echo "scale=$floating_point_scale; ($animation_start_time + $animation_end_time)/2" |bc`
+    
+    # echo "starttime "$animation_start_time
+    # echo "middletime "$animation_middle_time
+    # echo "endtime "$animation_end_time
 
     css_string="\n
     @keyframes animation-$imagefilenameprefix {\n
         \t 0% {opacity: 0; }\n
-        \t 57.14286% {opacity: 0; }\n
-        \t 71.42857% {opacity: 1; }\n
-        \t 85.71429% {opacity: 0; }\n
+        \t $animation_start_time% {opacity: 0; }\n
+        \t $animation_middle_time% {opacity: 1; }\n
+        \t $animation_end_time% {opacity: 0; }\n
         \t 100% {opacity: 0; } \n
     }\n
     .bg-container #bg-$imagefilenameprefix {\n
@@ -29,7 +42,7 @@ do
         \t min-width: 100%;\n
         \t min-height: 100%;\n
         \t background: no-repeat center center;\n
-        \t animation: animation-$imagefilenameprefix 35s ease-out infinite;\n
+        \t animation: animation-$imagefilenameprefix $animation_length_in_second"s" ease-out infinite;\n
         }\n
     @media screen and (min-width: 500px) {\n
         \t     .bg-container #bg-$imagefilenameprefix {\n
@@ -44,5 +57,6 @@ do
     echo $css_string
     # echo "<div id=\"bg-$imagefilenameprefix\"></div>"
 
+    loop_count=$(($loop_count+1))
 done
 
